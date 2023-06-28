61cd010ee3d2fe1972304bbf741738e42278c591
Gabrijel Cabraja <wi22b035@technikum-wien.at>
2023-06-28 19:59:38 +0200

    Initial commit

---
+++ StRiNgMoDiFy-API/src/main/java/at/technikum/StRiNgMoDiFyAPI/StRiNgMoDiFyApiApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.StRiNgMoDiFyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StRiNgMoDiFyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StRiNgMoDiFyApiApplication.class, args);
+	}
+
+}
---
+++ StRiNgMoDiFy-API/src/test/java/at/technikum/StRiNgMoDiFyAPI/StRiNgMoDiFyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.StRiNgMoDiFyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StRiNgMoDiFyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
